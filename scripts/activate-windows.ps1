5bc75cb36bf85055ccd5642f0c1153eab8e1c8ef
Caden F <121118774+MysticalMike60t@users.noreply.github.com>
2024-10-04 13:21:57 -0700

    Update activate-windows.ps1

--- scripts/activate-windows.ps1
+++ scripts/activate-windows.ps1
@@ -7,7 +7,7 @@ Write-Host "Changed Security Settings"
 Write-Host "Executing Script..."
 Write-Host "Credit: Massgravel | https://github.com/massgravel/Microsoft-Activation-Scripts"
 
-Invoke-WebRequest https://get.activated.win | Invoke-Expression
+Invoke-WebRequest "https://get.activated.win" | Invoke-Expression
 
 if ($?) {
     Write-Host "Activation Completed"
